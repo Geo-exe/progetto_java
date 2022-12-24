d71d3fe153f178f67f7176cee86f64d907d7eead
Francesco Griffa <65287940+Geo-exe@users.noreply.github.com>
2022-12-25 00:01:41 +0100

    descrizione
    Agenda e Appuntamento
    
    AgendaList sistemata
    
    Creazione di Validation
    
    Eliminazione di AgendaBox

--- project_agenda/src/graphic_user_interface/WindowAgenda.java
+++ project_agenda/src/graphic_user_interface/WindowAgenda.java
@@ -2,16 +2,24 @@ package graphic_user_interface;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
+
+import sourcecode.Agenda;
 
 public class WindowAgenda extends JFrame {
 	private static final long serialVersionUID = 1L;
 
 	public WindowAgenda() {
 		super("Dashboard");
-		setLayout(new BorderLayout());
-
-		AgendaList ciaone = new AgendaList();
+		setLayout(new GridLayout(2, 2));
+		ArrayList<Agenda> bho = new ArrayList<Agenda>();
+		bho.add(new Agenda("Ciaone"));
+		bho.add(new Agenda("Lavoro"));
+		bho.add(new Agenda("Casa"));
+		AgendaList ciaone = new AgendaList(bho);
 		
 		add(ciaone, BorderLayout.CENTER);
 
--- project_agenda/src/sourcecode/Appointment.java
+++ project_agenda/src/sourcecode/Appointment.java
@@ -1,5 +1,19 @@
 package sourcecode;
 
-public class Appointment {
+import java.sql.Time;
+import java.sql.Date;
 
+public class Appointment {
+	private String location;
+	private Date date;
+	private Time time;
+	private String person;
+	
+	public Appointment(Date date, Time time, String location, String person) {
+		
+	}
+	public Appointment(String date, String time, String location, String person) {
+		
+	}
+	
 }
