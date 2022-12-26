c2b341dd353a643b0f21baed7dcbd40739a1bc09
Francesco Griffa <65287940+Geo-exe@users.noreply.github.com>
2022-12-26 20:27:03 +0100

    sviluppo main

--- project_agenda/src/Main.java
+++ project_agenda/src/Main.java
@@ -5,23 +5,16 @@ import graphic_user_interface.WindowAgenda;
 import sourcecode.Agenda;
 
 public class Main {
-	public static void main(String[] args) throws ParseException {
-		
-		// ancora da implementare. Il codice seguente è solo di test.
-		
-		ArrayList<Agenda> ags = new ArrayList<Agenda>();
-		Agenda ag = new Agenda("Casa");
-		Agenda ag2 = new Agenda("Lavoro");
-		ag.addAppointment("12/12/2022", "12:30", "Ufficio", "Dario");
-		ag.addAppointment("12/12/2022", "12:30", "Ufficio", "Dario");
-		ag2.addAppointment("13/12/2022", "14:30", "Casa", "Mamma");
-		ag2.addAppointment("13/12/2022", "14:30", "Casa", "Mamma");
-		ag2.addAppointment("13/12/2022", "14:30", "Casa", "Mamma");
-		ag2.addAppointment("13/12/2022", "14:30", "Casa", "Mamma");
-		ag2.addAppointment("13/12/2022", "14:30", "Casa", "Mamma");
-		ag2.addAppointment("13/12/2022", "14:30", "Casa", "Mamma");
-		ags.add(ag);
-		ags.add(ag2);
-	    new WindowAgenda(ags);
+	
+	private ArrayList<Agenda> agendas;
+	
+	public void main() throws ParseException {
+		agendas = new ArrayList<Agenda>();
+		agendas.add(null);
+		new WindowAgenda(agendas);
 	  }
+	
+	private void addAgenda(String Nome) {
+		
+	}
 }
