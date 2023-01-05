45a1b1843568ce868dc4ba4b450a3b0ce0877003
fabietto2002 <20043879@studenti.uniupo.it>
2023-01-05 17:06:28 +0100

    Update DelAgendaWindow.java

--- project_agenda/src/graphic_user_interface/DelAgendaWindow.java
+++ project_agenda/src/graphic_user_interface/DelAgendaWindow.java
@@ -1,32 +1,73 @@
 package graphic_user_interface;
 
+import java.awt.GridLayout;
 import java.util.ArrayList;
 
+
+import javax.swing.DefaultListModel;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import sourcecode.Agenda;
+
 
 public class DelAgendaWindow extends ActionWindow {
 
 	private static final long serialVersionUID = 1L;
+	
+	private JComboBox<String> comboBox;
 
 	public DelAgendaWindow(String title, ArrayList<Agenda> agendas, JList<String> agendasList,
 			boolean actionWindowIsOpen) throws Exception {
 		super(title, agendas, agendasList, actionWindowIsOpen);
-		// TODO Auto-generated constructor stub
+		
 	}
 
+	
 	@Override
 	public void confirmAction() {
-		// TODO Auto-generated method stub
+
+		DefaultListModel<String> model = (DefaultListModel<String>) agendasList.getModel();
+
+		model.removeElement(comboBox.getSelectedItem());
+		
+		
+		for(int i=0;i<agendas.size();i++) {
+			if(agendas.get(i).getName().equals(comboBox.getSelectedItem())) {
+				agendas.remove(i);
+			}
+		}
+		
+		setVisible(false);
+		dispose();
+		JOptionPane.showMessageDialog(null, "Agenda "+ comboBox.getSelectedItem() +" eliminata!");
+		
+		
+		
 
 	}
 
 	@Override
 	protected JPanel loadFields() {
-		// TODO Auto-generated method stub
-		return null;
+		JPanel tempPanel = new JPanel();
+		tempPanel.setLayout(new GridLayout(2,2,5,5));
+		tempPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		tempPanel.add(new JLabel("Seleziona un' agenda da :"));
+		
+		comboBox= new JComboBox<String>();
+		
+		for(int i=0;i<agendas.size();i++) {
+			comboBox.addItem(agendas.get(i).getName());
+
+		}
+
+		tempPanel.add(comboBox);
+		
+		return tempPanel;
 	}
 
 }
