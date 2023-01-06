1710dfee35bf9093439e14ddc42982ba19de377a
Francesco Griffa <65287940+Geo-exe@users.noreply.github.com>
2023-01-07 00:27:30 +0100

    agenda iterabile YEEE
    senza rimozione elementi come da indicazioni.

--- project_agenda/src/sourcecode/Agenda.java
+++ project_agenda/src/sourcecode/Agenda.java
@@ -4,60 +4,79 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
 
 import utils.AppointmentUtils;
 
-public class Agenda {
+public class Agenda implements Iterable<Appointment> {
 	private String name;
 	private ArrayList<Appointment> appointments;
-	
-	public Agenda(String name){
-		if(name.equals("")) {
+
+	public Agenda(String name) {
+		if (name.equals("")) {
 			throw new IllegalArgumentException("Il Nome non può essere vuoto");
 		}
 		this.name = name;
 		this.appointments = new ArrayList<Appointment>();
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public ArrayList<Appointment> getAppointments() {
 		return appointments;
 	}
-	
+
 	public int size() {
 		return appointments.size();
 	}
-	
+
 	public void addAppointment(Calendar date_time, String location, String person, int duration) throws ParseException {
-		if(AppointmentUtils.checkAvailability(date_time, location, person, duration,appointments)) {
+		if (AppointmentUtils.checkAvailability(date_time, location, person, duration, appointments)) {
 			this.appointments.add(new Appointment(date_time, location, person, duration));
 		}
-		
+
 	}
-	
+
 	public void removeAt(int i) {
 		appointments.remove(i);
 	}
-	
+
 	public void removeAll() {
-		
+
 		appointments.clear();
 	}
-	
+
 	public void modifyAppointment(Calendar date_time, String location, String person, String duration) {
-		
-		
+
 	}
-	
+
 	public ArrayList<Appointment> sortAppointmets(OrderMethodEnum selectedMethod) {
 		ArrayList<Appointment> result = new ArrayList<Appointment>(appointments);
 		selectedMethod.orderByDate(result);
 		return result;
 	}
-	
-	
-	
+
+	@Override
+	public Iterator<Appointment> iterator() {
+		return new Iterator<Appointment>() {
+			private Iterator<Appointment> iterator = appointments.iterator();
+
+			@Override
+			public boolean hasNext() {
+				return iterator.hasNext();
+			}
+
+			@Override
+			public Appointment next() {
+				return iterator.next();
+			}
+
+			@Override
+			public void remove() {
+				throw new UnsupportedOperationException("Impossibile rimuovere elementi durante l'iterazione");
+			}
+		};
+	}
 }
