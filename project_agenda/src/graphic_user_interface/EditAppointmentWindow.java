ad41431d668c5a371b2cad39800062de5a168520
fabietto2002 <20043879@studenti.uniupo.it>
2023-01-06 02:30:27 +0100

    prima parte di editappointment

--- project_agenda/src/graphic_user_interface/EditAppointmentWindow.java
+++ project_agenda/src/graphic_user_interface/EditAppointmentWindow.java
@@ -1,18 +1,30 @@
 package graphic_user_interface;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.border.EmptyBorder;
 
 import sourcecode.Agenda;
+import sourcecode.Appointment;
 
 public class EditAppointmentWindow extends ActionWindow {
-
+	
+	private static final long serialVersionUID = 1L;
+	private JRadioButton[] radioButton;
+	
 	public EditAppointmentWindow(String title, ArrayList<Agenda> agendas, JList<String> agendasList,
 			boolean actionWindowIsOpen) throws Exception {
 		super(title, agendas, agendasList, actionWindowIsOpen);
-		// TODO Auto-generated constructor stub
+		confirm.setText("Modifica");
+		setBounds(0, 0, 350, 650);
+		setLocationRelativeTo(null);
 	}
 
 	@Override
@@ -23,8 +35,46 @@ public class EditAppointmentWindow extends ActionWindow {
 
 	@Override
 	protected JPanel loadFields() {
-		// TODO Auto-generated method stub
-		return null;
+		JPanel tempPanel = new JPanel();
+		ArrayList<Appointment> result;
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		result=agendas.get(agendasList.getSelectedIndex()).getAppointments();
+		
+		if(!result.isEmpty()) {
+		
+		tempPanel.setLayout(new GridBagLayout());
+		tempPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		gbc.gridy=0;
+		tempPanel.add(new JLabel("Seleziona un appuntamento:"),gbc);
+		
+		
+		radioButton = new JRadioButton[result.size()]; 
+
+		for(int i = 0; i < result.size(); i++) {
+			radioButton[i] = new JRadioButton();
+			
+			radioButton[i].addActionListener(e->{
+				
+				for(int j=0;j<radioButton.length;j++) {
+					if(!radioButton[j].equals(e.getSource()) ) {
+						radioButton[j].setSelected(false);
+					}
+				}
+				
+			});
+			
+			gbc.gridy = i+2;
+			tempPanel.add(radioButton[i],gbc);
+			tempPanel.add(new AppointmentBox(result.get(i)),gbc);
+			
+		}
+		
+		}else {
+			tempPanel.add(new JLabel("Nessun appuntamento!"));
+		}
+		
+		return tempPanel;
 	}
 
 }
